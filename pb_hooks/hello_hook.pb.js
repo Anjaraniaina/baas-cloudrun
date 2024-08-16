f868e9f5c2ed044092bcdafdb584029a192aa902
Anjaraniaina <hei.anjaraniaina@gmail.com>
2024-08-16 17:48:02 +0300

    feat: add logs

--- pb_hooks/hello_hook.pb.js
+++ pb_hooks/hello_hook.pb.js
@@ -1,7 +1,8 @@
 routerAdd('GET', '/hello/:name', (c) => {
     const name = c.pathParam('name')
+    console.log("Endpoint was hit" + name)
   
     return c.json(200, {
       name,
     })
-  })
+  }, false)
