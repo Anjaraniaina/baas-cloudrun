0199eff7b2ec08c5f834f3bdd96eec9dd6227127
Anjaraniaina <hei.anjaraniaina@gmail.com>
2024-08-16 18:55:34 +0300

    Revert "feat: add logs"
    This reverts commit f868e9f5c2ed044092bcdafdb584029a192aa902.

--- pb_hooks/hello_hook.pb.js
+++ pb_hooks/hello_hook.pb.js
@@ -1,8 +1,7 @@
 routerAdd('GET', '/hello/:name', (c) => {
     const name = c.pathParam('name')
-    console.log("Endpoint was hit" + name)
   
     return c.json(200, {
       name,
     })
-  }, false)
+  })
